00597ec8e5c7a36c7975f59a73b2524104c5e151
hanock <htsegaye@terpmail.umd.edu>
2023-04-07 23:20:32 -0400

    next button implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import SearchBar from './components/SearchBar';
-import NextButton from './components/NextButton';
+import SearchBar from './components/searchBar';
+import NextButton from './components/nextButton';
 import TextField from '@mui/material/TextField';
 
 
 function App() {
   const [majors, setMajors] = useState(["some default val", "val 3"]);
+  const [toCourses, setToCourses] = useState(false);
+  const [findCourses, setFindCourses] = useState(false);
 
   useEffect(() => {
     console.log(majors);
@@ -16,13 +18,24 @@ function App() {
     <div className="App">
       <h1>RecommendMe</h1>
       <h2>Insert Major (Department Code)</h2>
-      <SearchBar searchType="major" values={majors} setValue={setMajors}/>
-      <NextButton />
-      <SearchBar searchType="major" values={majors} setValue={setMajors}/>
-      <NextButton />
-
-      <h3>Find Courses</h3>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <SearchBar searchType="major" values={majors} setValue={setMajors} />
+      <NextButton value={toCourses} setValue={setToCourses} />
+      <div> 
+        {toCourses ?
+          <>
+            <SearchBar searchType="major" values={majors} setValue={setMajors} />
+            <NextButton value ={findCourses} setValue={setFindCourses}/>
+          </> : ""
+        }
+      </div>
+      <div>
+        {findCourses ?
+          <>
+            <h3>Find Courses</h3>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </> : ""
+        }
+      </div>
     </div>
   );
 }
--- frontend/src/components/nextButton.js
+++ frontend/src/components/nextButton.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Button from '@mui/material/Button';
 
 
-function NextButton() {
+function NextButton(props) {
   return (
-    <Button variant="contained">Next</Button>
-    )
+    <Button variant="contained" onClick={(event) => {
+      props.setValue(!props.value)
+    }}>{!props.value ? "Next" : "Previous"}</Button>
+  )
 }
 
 export default NextButton
