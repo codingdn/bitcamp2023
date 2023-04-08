1a607b246490f7440c3cf4ca4b910371c7ae0a2f
Dan Nguyen <danthedevnguyen@gmail.com>
2023-04-08 13:03:58 -0400

    centered cards

--- frontend/src/components/courseCard.js
+++ frontend/src/components/courseCard.js
@@ -2,40 +2,42 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
+import Box from "@mui/material/Box"
 
 function CourseCard(props) {
   
 
   return (
-    <Card>
-      <h3>{props.course.course_id}</h3>
-      <CardContent>
-        <h5>
-          {props.course.course_id} - {props.course.name}
-        </h5>
-        <p>Department: {props.course.dept_id}</p>
-        <p>{props.course.description}</p>
-        <ul>
-          <li>Prereqs: {props.course.prereqs}</li>
-          <li>Restrictions: {props.course.restrictions}</li>
-          <li>Additional Info: {props.course.additional_info}</li>
-          <li>Credit Granted For: {props.course.credit_granted_for}</li>
-        </ul>
-        
-        {props.course.gened.length > 0 ? (
-          <div>
-            <h3>Gened</h3>
-            <ul>
-              {props.course.gened.map((element, idx) => (
-                <p>{element.toString()}</p>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          ""
-        )}
-      </CardContent>
-    </Card>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      marginTop="20px"
+    >
+      <Card
+        sx={{
+          width: 600,
+        }}
+      >
+        <CardContent>
+          <h3>
+            {props.course.course_id} - {props.course.name}
+          </h3>
+          <p>{props.course.description}</p>
+          <ul>
+            <li>Prereqs: {props.course.prereqs}</li>
+            {!props.course.restrictions === "" ? (
+              <li>Restrictions: {props.course.restrictions}</li>
+            ) : null}
+            <li>Additional Info: {props.course.additional_info}</li>
+            <li>Credit Granted For: {props.course.credit_granted_for}</li>
+            {props.course.gened.length > 0 ? (
+              <li>Gened: {props.course.gened.toString()}</li>
+            ) : null}
+          </ul>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
