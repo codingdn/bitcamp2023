d7be28dbd6c4ebb1110c26a52ff6ed6531465ec2
Dan Nguyen <danthedevnguyen@gmail.com>
2023-04-07 21:31:51 -0400

    working on search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <h1>RecommendMe</h1>
+      <SearchBar/>
     </div>
   );
 }
--- frontend/src/components/searchBar.js
+++ frontend/src/components/searchBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import majors from '../data/majors.json';
 
 function searchBar() {
-  return (
-    <div>searchBar</div>
-  )
+    return (
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={majors.majors}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Movie" />}
+        />
+      );
 }
 
 export default searchBar
